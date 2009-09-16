f2666c7f8e684f77f02f716b2b958b7dcfbf129a
ilich.ramirez@gmail.com <ilich.ramirez@gmail.com@126b0e36-4d07-11de-aa6a-4703752c21e7>
2009-09-16 19:37:44 +0000

    wip

--- trunk/seriesManager/scopritore-c.py
+++ trunk/seriesManager/scopritore-c.py
@@ -9,7 +9,9 @@ class Scopritore(QtGui.QMainWindow):
 		self.ui = Ui_MainWindow()
 		self.ui.setupUi(self)
 		QtCore.QObject.connect(self.ui.azione, QtCore.SIGNAL("currentIndexChanged(QString)"), self.disablePath)
+		QtCore.QObject.connect(self.ui.pushButton, QtCore.SIGNAL("returnPressed()"), self.scopri)
 		QtCore.QObject.connect(self.ui.pushButton, QtCore.SIGNAL("clicked()"), self.scopri)
+
 		screen = QtGui.QDesktopWidget().screenGeometry()
 		size =  self.geometry()
 		self.move((screen.width()-size.width())/2, (screen.height()-size.height())/2)
@@ -34,16 +36,32 @@ class Scopritore(QtGui.QMainWindow):
             l=[i for i in sc.find(r,sc.TOR)]
             if  len(l)==0:
                 QtGui.QMessageBox.warning(self, 'Avviso',"Nessun file video trovato %s %s %s %s" % (serie,S,E,formato))
-                
             else:
                 if len(l)==1:
                     video=l[0]
                 else:
                     video, ok = QtGui.QInputDialog.getItem(None, 'Input Dialog', 'Scegli video',l)
                 if video:
-                    QtGui.QMessageBox.warning(self, 'Avviso',"Trovato "+video)
-            
-            
+					r=sc.make_pattern(serie,S,E,"zip")
+					l=[i for i in sc.find(r,sc.SUBS)]
+					if  len(l)==0:
+						QtGui.QMessageBox.warning(self, 'Avviso',"Nessun file zip trovato %s %s %s %s" % (serie,S,E,formato))						
+					else:
+						if len(l)==1:
+							zip=l[0]
+						else:
+							zip, ok = QtGui.QInputDialog.getItem(None, 'Input Dialog', 'Scegli video',l)
+						if zip:
+							srts=sc.unzip(sc.SUBS+"/"+zip)
+							if len(srts)>1:
+								srt, ok = QtGui.QInputDialog.getItem(None, 'Input Dialog', 'Scegli sottotitolo',srts)
+							else:
+								srt=srts[0]
+							if srt:
+								sc.rename(sc.TOR+"/"+video,srt)
+								if self.ui.azione.currentText()== 'vlc':
+									print os.popen("vlc "+video)
+								
             
 
 
