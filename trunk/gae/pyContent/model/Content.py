3ffa8e8e0e5bbfe1e0f4fb0d637d5e448ca5964a
ilich.ramirez <ilich.ramirez@126b0e36-4d07-11de-aa6a-4703752c21e7>
2010-12-13 23:41:22 +0000

    aggiunti test level, ancestor, nomi duplicati implementati ancestor, level

--- trunk/gae/pyContent/model/Content.py
+++ trunk/gae/pyContent/model/Content.py
@@ -7,12 +7,12 @@ class Content(db.Expando):
     creationDate=db.DateTimeProperty(required=True)
 
     def __init__(self, parent=None, key_name=None, _app=None, **kwds):
-        kwds['name']=self.sanitize(kwds["name"])
+        kwds['name']=ContentUtils.sanitize(kwds["name"])
         kwds['creationDate']=db.DateTimeProperty.now()
         db.Expando.__init__(self,parent=parent, key_name=key_name, _app=None, **kwds)
         
     def setName(self,name):
-        self.name__=self.sanitize(name)
+        self.name__=ContentUtils.sanitize(name)
         
     def getName(self):
         return self.name__
@@ -28,16 +28,34 @@ class Content(db.Expando):
         return q.ancestor(self.key()).filter('__key__ !=',self.key())
         
     def ancestor(self,level):
+        if self.level==level:
+            return self
+        elif level > self.level:
+            raise PathNotFoundException()
+        elif level <= 0:
+            raise PathNotFoundException()
+        else:
+            return self.parent().ancestor(level)
         #check magnolia/jcr code
-        if level==1: 
+        '''if level==1: 
             return self
         elif self.parent():
             return self.parent().ancestor(level-1)
-    
+        '''
+        #raise MethodNotYetImplemented()
+
+    def getLevel(self):
+        if self.parent():
+            return self.parent().getLevel() + 1
+        else:
+            return 1
+        
     name=property(fget=getName,fset=setName)
     handle=property(fget=getHandle)
     children=property(fget=getChildren)
+    level=property(fget=getLevel)
     
+    '''
     def sanitize(self,name):
         sanitized_name=ContentUtils.sanitize(name)
         if (self.parent() and sanitized_name in [c.name for c in self.parent().children]):
@@ -48,8 +66,7 @@ class Content(db.Expando):
                 index=0
             return self.sanitize("%s%s" % (m.group(1),index))
         return sanitized_name
-                
-            
+       '''
     
 class ContentUtils(object):
     @staticmethod
@@ -58,3 +75,9 @@ class ContentUtils(object):
     
 class DuplicateContentNameException(Exception):
     """Duplicate named content at the same level"""
+
+class MethodNotYetImplementedException(Exception):
+    """This method is not yet implemented"""
+    
+class PathNotFoundException(Exception):
+    """Path not found"""
