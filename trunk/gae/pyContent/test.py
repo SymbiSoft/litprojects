1687a1aaf96393de8ec16ac7fb7722882d00bcc1
ilich.ramirez <ilich.ramirez@126b0e36-4d07-11de-aa6a-4703752c21e7>
2010-12-13 22:34:23 +0000

    aggiunti test

--- trunk/gae/pyContent/test.py
+++ trunk/gae/pyContent/test.py
@@ -1,4 +1,7 @@
 import unittest
+import logging
+
+import pdb
 
 from model.Content import Content
 #import unittest.TestCase as t
@@ -35,11 +38,26 @@ class ContentModelTest(unittest.TestCase):
         c2=Content(name="children2",parent=p)
         c1.put()
         c2.put()
-        children=p.children
-        self.assertTrue(c1 in children and c2 in children)
+        children=[c.key() for c in p.children]
+        self.assertEquals(len(children),2)
+        self.assertTrue(c1.key() in children and c2.key() in children)
+        c1.delete()
+        c2.delete()
+        p.delete()
+        
         
     def handle_test(self):
         p=Content(name="parent")
         p.put()
         c=Content(name="children",parent=p)
         self.assertEquals(c.handle,"/parent/children")
+
+    def ancestor__disabled(self):
+        gp=Content(name="grandpa_ancestor")
+        gp.put()
+        p=Content(name="parent_ancestor",parent=gp)
+        p.put()
+        c=Content(name="child_ancestor",parent=p)
+        c.put()
+        self.assertEquals(c.ancestor(1).key() ,gp.key())        
+        
